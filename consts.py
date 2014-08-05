97e2ce3e2bfbce6583490ede2ef6bb23fd555380
sfabian <me@sfabian.net>
2014-08-05 17:26:54 +0200

    Now there are four games, running in parallell next to each other :)

--- consts.py
+++ consts.py
@@ -5,7 +5,12 @@ cell_size = 16
 game_size = game_width, game_height = 16, 9 
 
 # in pixels
-screen_size = width, height = game_width * cell_size, game_height * cell_size
+game_screen_size = game_screen_width, game_screen_height = game_width * cell_size, game_height * cell_size
+
+# in pixels
+# 2, because we want 4 games on one screen
+screen_size = width, height = 2 * game_screen_width, 2 * game_screen_height
 
 # colors
 black = (0, 0, 0)
+white = (255, 255, 255)
--- game.py
+++ game.py
@@ -11,75 +11,88 @@ screen = pygame.display.set_mode(screen_size)
 timer = pygame.time.Clock()
 fps = 5
 
-cells = []
+class Game:
+  def __init__(self):
+    self.__init_randomized_cells()
+    self.surface = pygame.Surface(screen_size)
 
-def neighbours(cell, cells):
-  iter_start_x = cell.x - 1
-  iter_start_y = cell.y - 1
-  iter_end_x = (cell.x + 1) % game_width
-  iter_end_y = (cell.y + 1) % game_height
- 
-  # corner cases
- # if cell.x == 0:
- #   iter_start_x = cell.x
- # if cell.y == 0:
- #   iter_start_y = cell.y
- # if cell.x == game_width-1:
- #   iter_end_x = cell.x
- # if cell.y == game_height-1:
- #   iter_end_y = cell.y
-  
-  neighbours = []
-  for xi in range(iter_start_x, iter_end_x+1):
-    for yi in range(iter_start_y, iter_end_y+1):
-      if cell.x == xi and cell.y == yi:
-        continue
-      neighbours.append(cells[yi][xi])
+  def __init_randomized_cells(self):
+    self.cells = []
 
-  return neighbours
+    for y in range(game_height):
+      row = []
+      self.cells.append(row)
 
-def alive_neighbours(cell, cells):
-  return filter(lambda c: c.is_alive(), neighbours(cell, cells))
+      for x in range(game_width):
+        row.append(Cell(x, y, random.choice([True, False])))
 
-
-def init_randomized_cells():
-  for y in range(game_height):
-
-    row = []
-    cells.append(row)
+  def iterate(self):
+    self.surface.fill(black)
 
     for x in range(game_width):
-      row.append(Cell(x, y, random.choice([True, False])))
+      for y in range(game_height):
+        cell = self.cells[y][x]
 
-def main_loop():
+        if cell.alive:
+          pygame.draw.rect(self.surface, cell.color(), cell.rect)
+
+        cell.calculate_next(len(self.__alive_neighbours(cell)))
+
+    for x in range(game_width):
+      for y in range(game_height):
+        cell = self.cells[y][x]
+        cell.new_generation()
+
+  def __neighbours(self, cell):
+    iter_start_x = cell.x - 1
+    iter_start_y = cell.y - 1
+    iter_end_x = (cell.x + 1) % game_width
+    iter_end_y = (cell.y + 1) % game_height
+   
+    # corner cases
+   # if cell.x == 0:
+   #   iter_start_x = cell.x
+   # if cell.y == 0:
+   #   iter_start_y = cell.y
+   # if cell.x == game_width-1:
+   #   iter_end_x = cell.x
+   # if cell.y == game_height-1:
+   #   iter_end_y = cell.y
+    
+    neighbours = []
+    for xi in range(iter_start_x, iter_end_x+1):
+      for yi in range(iter_start_y, iter_end_y+1):
+        if cell.x == xi and cell.y == yi:
+          continue
+        neighbours.append(self.cells[yi][xi])
+
+    return neighbours
+
+  def __alive_neighbours(self, cell):
+    return filter(lambda c: c.is_alive(), self.__neighbours(cell))
+
+
+def main_loop(games):
   while True:
     timer.tick(fps)
 
     screen.fill(black)
+    
+    for i in range(4):
+      games[i].iterate()
+      screen.blit(games[i].surface, ((i%2)*game_screen_width,([0,0,1,1][i])*game_screen_height))
 
-    for x in range(game_width):
-      for y in range(game_height):
-        cell = cells[y][x]
-
-        if cell.alive:
-          pygame.draw.rect(screen, cell.color(), cell.rect)
-
-        cell_age = cell.calculate_next(len(alive_neighbours(cell, cells)))
-        if cell_age != -1:
-          # play a note
-          print cell_age
-
-    for x in range(game_width):
-      for y in range(game_height):
-        cell = cells[y][x]
-        cell.new_generation()
+    pygame.draw.line(screen, white, (0, game_screen_height), (2*game_screen_width, game_screen_height))
+    pygame.draw.line(screen, white, (game_screen_width, 0), (game_screen_width, 2*game_screen_height))
 
     pygame.display.flip()
 
 def main(): 
   pygame.init()
-  init_randomized_cells()
-  main_loop()
+  games = []
+  for i in range(4):
+    games.append(Game())
+  main_loop(games)
 
 if __name__ == '__main__':
   main()
