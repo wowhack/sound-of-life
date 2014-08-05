dbb727a1c00ac48ca84c5ca31ef728a34f56106c
sfabian <me@sfabian.net>
2014-08-05 13:24:59 +0200

    Now the cell ages

--- cell.py
+++ cell.py
@@ -22,6 +22,7 @@ class Cell:
       elif no_of_alive_neighbours == 2 or no_of_alive_neighbours == 3: 
         # 'enough neighbours to live on'
         self.alive_next = True
+        self.__age()
       elif no_of_alive_neighbours > 3:
         # 'death by over-population'
         self.alive_next = False
@@ -30,6 +31,8 @@ class Cell:
       if no_of_alive_neighbours == 3:
         # 'regenerate by reproduction'
         self.alive_next = True
+        age = 0
+        self.age()
       else:
         # 'stay dead :('
         self.alive_next = False
