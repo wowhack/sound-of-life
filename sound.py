4626630984547d038d625bab28972112a2f1c37a
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2014-08-05 15:51:24 +0200

    Play some note

--- sound.py
+++ sound.py
@@ -3,20 +3,32 @@ from pyo import *
 class Sound:
   def __init__(self):
     self.s = Server().boot()
-
-    self.trig = Trig()
-
     self.env = CosTable([(0,0),(50,1),(500,.25),(8191,0)])
     self.wav = SquareTable(5)
-
-    self.trigenv = TrigEnv(input = self.trig, table = self.env, dur = 0.250)
-
-    self.osc = Osc(table = self.wav, freq = 440, mul = self.trigenv).out()
-    self.osc2 = Osc(table = self.wav, freq = 440, mul = self.trigenv).out(1)
-
     self.s.start()
 
+    self.freqs = {}
+
+    self.addfreq(440)
+
+  def addfreq(self, freq):
+    self.freqs[freq] = Freq(freq, self.env, self.wav)
+
+  def play(self, freq):
+    """Trigger a tone, note, signal, whatever"""
+    self.freqs[freq].play()
+
+
+class Freq:
+  def __init__(self, freq, env, wav):
+    self.trig = Trig()
+
+    self.trigenv = TrigEnv(input = self.trig, table = env, dur = 0.250)
+
+    self.osc = Osc(table = wav, freq = freq, mul = self.trigenv).out()
+    self.osc2 = Osc(table = wav, freq = freq, mul = self.trigenv).out(1)
 
   def play(self):
-    """Trigger a tone, note, signal, whatever"""
     self.trig.play()
+
+s = Sound()
