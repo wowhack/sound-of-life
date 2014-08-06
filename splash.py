1c260bf9805f3568759d46f82f2605c3f14ff887
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2014-08-06 10:42:06 +0200

    Splash is beautiful. Accept defeat.

--- splash.py
+++ splash.py
@@ -4,39 +4,37 @@ from pygame.locals import *
 from cell import Cell
 from consts import *
 
-
 class Splash:
-  def __init__(self, c):
+  def __init__(self):
     self.__init_randomized_cells()
-    self.c = ((180, 73, 74), (75, 182, 181))
     self.surface = pygame.Surface(screen_size)
 
   def __init_randomized_cells(self):
     self.cells = []
 
-    for y in range(game_height*2):
+    for y in range(game_height*2 - 1):
       row = []
       self.cells.append(row)
 
-      for x in range(game_width*2):
+      for x in range(game_width*2 - 1):
         row.append(Cell(x, y, ((180, 180, 0), (75, 182, 181)), random.choice([True, False])))
 
   def draw(self):
     self.surface.fill(black)
     for cs in self.cells:
-        for c in cs:
-          if c.alive:
-            pygame.draw.rect(self.surface, c.color, c.rect)
+      for c in cs:
+        if c.alive:
+          pygame.draw.rect(self.surface, c.color, c.rect)
 
   def iterate(self):
     for cs in self.cells:
-        for c in cs:
-          c.calculate_next(len(self.__alive_neighbours(c)))
+      for c in cs:
+        c.calculate_next(len(self.__alive_neighbours(c)))
 
   def new_generation(self):
     for cs in self.cells:
-        for cell in cs:
-          cell.new_generation()
+      for cell in cs:
+        cell.new_generation()
 
   def __neighbours(self, cell):
     iter_start_x = cell.x - 1
