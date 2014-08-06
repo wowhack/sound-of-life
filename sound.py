8964b82e1af555b8957234c842a54aae9f07bce7
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2014-08-06 14:13:30 +0200

    ressa gammal kod!

--- sound.py
+++ sound.py
@@ -3,44 +3,26 @@ from consts import *
 
 class Sound:
   def __init__(self, freqs, vol):
-    self.shape = CosTable([(0,0),(50,1),(500,.1),(8191,0)])
-    self.wave = SawTable(12)
+    self.env = CosTable([(0,0),(50,1),(500,.25),(8191,0)])
+    self.wav = SawTable(12)
 
-    self.vol = vol
-    self.freqs = freqs
-    self.q = []
-    self.c = 10
-    self.curr = 0
-    self.table = [TableRead(table=self.shape, freq=1 / beat_duration, mul = self.vol) for i in range(self.c)]
-    self.gen = [Sine(mul = table).out() for table in self.table]
-    #self.freqs = []
-    #for f in freqs:
-      #self.freqs.append(Freq(f, self.shape, self.wave, vol))
+    self.freqs = []
+    for f in freqs:
+      self.freqs.append(Freq(f, self.env, self.wav, vol))
 
   def play(self, freq):
     """Play a note"""
-    #gen = self.gen[self.curr]
-    #table = self.table[self.curr]
-    #gen.freq = self.freqs[freq % len(self.freqs)]
-    #table.play()
-    #self.curr = (self.curr + 1) % self.c
-    self.q.extend([
-        Sine(freq = self.freqs[freq % len(self.freqs)], mul = self.vol).out(dur = beat_duration),
-        #Sine(freq = 2*self.freqs[freq % len(self.freqs)], mul = self.vol).out(dur = beat_duration),
-        Sine(freq = self.freqs[freq % len(self.freqs)], mul = self.vol).out(1, dur = beat_duration),
-        #Sine(freq = 2*self.freqs[freq % len(self.freqs)], mul = self.vol).out(1, dur = beat_duration)
-      ])
-    #self.freqs[freq % len(self.freqs)].play()
+    self.freqs[freq % len(self.freqs)].play()
 
 
 class Freq:
-  def __init__(self, freq, shape, wave, vol):
+  def __init__(self, freq, env, wav, vol):
     self.trig = Trig()
 
-    self.trigenv = TrigEnv(input = self.trig, table = shape, dur = beat_duration*0.7)
+    self.trigenv = TrigEnv(input = self.trig, table = env, dur = beat_duration)
 
-    self.osc = Osc(table = wave, freq = freq, mul = self.trigenv * vol).out()
-    self.osc2 = Osc(table = wave, freq = freq, mul = self.trigenv * vol).out(1)
+    self.osc = Osc(table = wav, freq = freq, mul = self.trigenv * vol).out()
+    self.osc2 = Osc(table = wav, freq = freq, mul = self.trigenv * vol).out(1)
 
   def play(self):
     self.trig.play()
